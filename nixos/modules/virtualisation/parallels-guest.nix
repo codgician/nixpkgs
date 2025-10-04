9a001f0d8f2abb442321e3ca0b9b9e1949abd54f
codgician <15964984+codgician@users.noreply.github.com>
2025-10-04 22:59:48 +0800

    prl-tools: 26.0.1-57243 -> 26.1.1-57288

--- nixos/modules/virtualisation/parallels-guest.nix
+++ nixos/modules/virtualisation/parallels-guest.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
@@ -27,20 +28,11 @@ in
         type = types.bool;
         default = false;
         description = ''
-          This enables Parallels Tools for Linux guests, along with provided
-          video, mouse and other hardware drivers.
+          This enables Parallels Tools for Linux guests.
         '';
       };
 
-      package = mkOption {
-        type = types.nullOr types.package;
-        default = config.boot.kernelPackages.prl-tools;
-        defaultText = "config.boot.kernelPackages.prl-tools";
-        example = literalExpression "config.boot.kernelPackages.prl-tools";
-        description = ''
-          Defines which package to use for prl-tools. Override to change the version.
-        '';
-      };
+      package = lib.mkPackageOption pkgs "prl-tools" { };
     };
 
   };
@@ -51,12 +43,6 @@ in
 
     environment.systemPackages = [ prl-tools ];
 
-    boot.extraModulePackages = [ prl-tools ];
-
-    boot.kernelModules = [
-      "prl_tg"
-    ];
-
     services.timesyncd.enable = false;
 
     systemd.services.prltoolsd = {
@@ -65,7 +51,7 @@ in
       path = [ prl-tools ];
       serviceConfig = {
         ExecStart = "${prl-tools}/bin/prltoolsd -f";
-        PIDFile = "/var/run/prltoolsd.pid";
+        PIDFile = "/run/prltoolsd.pid";
         WorkingDirectory = "${prl-tools}/bin";
       };
     };
